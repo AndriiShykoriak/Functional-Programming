45381b6cbf5a1545892f727fb85b0c4e229cb8f1
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-07-17 11:49:42 +0300

    refactored from cyrillic words to english

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -6,13 +6,13 @@ import java.util.List;
 public record User(String name, int age, String city) {
     public static List<User> getUserList() {
         List<User> userList = new ArrayList<>();
-        userList.add(new User("Вася", 16, "Днепр"));
-        userList.add(new User("Петя", 23, "Днепр"));
-        userList.add(new User("Елена", 42, "Луцк"));
-        userList.add(new User("Елена", 92, "Чернигов"));
-        userList.add(new User("Сергей", 5, "Киев"));
-        userList.add(new User("Марина", 32, "Киев"));
-        userList.add(new User("Иван Иванович", 69, "Львов"));
+        userList.add(new User("Vasia", 16, "Dnipro"));
+        userList.add(new User("Petia", 23, "Dnipro"));
+        userList.add(new User("Olena", 42, "Lutsk"));
+        userList.add(new User("Olena", 92, "Chernihiv"));
+        userList.add(new User("Serhii", 5, "Kiev"));
+        userList.add(new User("Marina", 32, "Kiev"));
+        userList.add(new User("Ivan Ibanovych", 69, "Lviv"));
         return userList;
     }
 }
